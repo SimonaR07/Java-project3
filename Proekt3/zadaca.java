8808407ffeafd2ddea892e9f5d3e1ba9ae7f0294
SimonaR07 <sristomanova32@gmail.com>
2023-12-05 18:25:59 +0100

    Add changes

---
+++ Proekt3/zadaca.java
@@ -0,0 +1,13 @@
+package Proekt3;
+
+public class zadaca {
+
+	public static void main(String[] args) {
+		zadaca dog = new zadaca();
+		dog.bark();
+		zadaca resi=new zadaca();
+		resi.zadaca();
+
+	}
+
+}
